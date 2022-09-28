05cf766914ad902031a4616806a21f718237998b
dpris <dmitry@snov.io>
2022-09-28 21:35:33 +0300

    CRUD part 2 / Work with SQL in Django models

--- my_app/news/models.py
+++ my_app/news/models.py
@@ -16,8 +16,11 @@ is_published - boolean
 
 class News(models.Model):
     title = models.CharField(max_length=150)
-    content = models.TextField(blank=True) # content can be kept blank. In the database ("") will be stored.
+    content = models.TextField(blank=True)  # content can be kept blank. In the database ("") will be stored.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d")
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
