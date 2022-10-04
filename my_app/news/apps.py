819f4211c6889f66d9e245f501f54685feb3a709
dpris <dmitry@snov.io>
2022-10-04 21:12:01 +0300

    admin django panel / add views.py / add apps.py / add admin.py / add models.py

--- my_app/news/apps.py
+++ my_app/news/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
+# Config my application
+# verbose_name = "All news" view in Django admin panel
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
+    verbose_name = "All news"
--- my_app/news/models.py
+++ my_app/news/models.py
@@ -15,12 +15,19 @@ is_published - boolean
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)  # content can be kept blank. In the database ("") will be stored.
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name="Title")
+    content = models.TextField(blank=True,
+                               verbose_name="Content")  # content can be kept blank. In the database ("") will be stored.
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name="Photo", blank=True)
+    is_published = models.BooleanField(default=True, verbose_name="Is published?")
 
     def __str__(self):
         return self.title
+
+    # Meta data in Django admin panel
+    class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "All News"
+        ordering = ["-created_at"]
