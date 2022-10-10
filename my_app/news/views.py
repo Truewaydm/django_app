9d7a22fdc88e7030784c505da8303b2948a483d9
dpris <dmitry@snov.io>
2022-10-10 17:26:33 +0300

    parameters in urls request

--- my_app/news/views.py
+++ my_app/news/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 # Create your views here.
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     # news = News.objects.order_by("-created_at")
-    context: dict = {"news": news, "title": "List of News"}
+    context: dict = {
+        "news": news,
+        "title": "List of News",
+        "categories": categories,
+    }
     # return render(request, "news/index.html", context)
     return render(request, template_name="news/index.html", context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
