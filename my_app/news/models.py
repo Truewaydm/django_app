9f77e41a56ae22e6e4d1a74cb71bd666488762d4
dpris <dmitry@snov.io>
2022-10-12 18:48:10 +0300

    working with forms part3

--- my_app/news/models.py
+++ my_app/news/models.py
@@ -23,7 +23,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name="Photo", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="Is published?")
-    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, verbose_name="Name of category")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, verbose_name="Name of category")
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})
