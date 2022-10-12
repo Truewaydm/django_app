56f10a7d96c40a6699d8be39033cf926c8f391cd
dpris <dmitry@snov.io>
2022-10-12 10:57:08 +0300

    custom template tags

--- my_app/news/views.py
+++ my_app/news/views.py
@@ -6,12 +6,10 @@ from .models import News, Category
 # Create your views here.
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     # news = News.objects.order_by("-created_at")
     context: dict = {
         "news": news,
         "title": "List of News",
-        "categories": categories,
     }
     # return render(request, "news/index.html", context)
     return render(request, template_name="news/index.html", context=context)
@@ -19,6 +17,5 @@ def index(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
